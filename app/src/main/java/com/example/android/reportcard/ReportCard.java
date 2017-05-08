1bdda43aad4fc6c7439a6f7f64d424dae0365270
Karel Nohava <karnoha@gmail.com>
2017-05-08 23:41:53 +0200

    code done 1.0

--- app/src/main/java/com/example/android/reportcard/ReportCard.java
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -1,13 +1,56 @@
 package com.example.android.reportcard;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class ReportCard extends AppCompatActivity {
+import java.util.Arrays;
+
+public class ReportCard {
+
+    /////////////////////// declaration of variables
+    // array of subjects
+    private String mSubject[];
+    // array of grades
+    private char mGrade[];
+    // int with a year we set
+    private int mYear;
+
+    // setter methods
+    public void setSubjects(String[] Subjects) {
+        this.mSubject = Subjects;
+    }
+
+    public void setGrades(char[] Grades) {
+        this.mGrade = Grades;
+    }
+
+    public void setYear(int Year) {
+        this.mYear = Year;
+    }
+
+    // getter methods
+    public String[] getSubjects() {
+        return mSubject;
+    }
+
+    public char[] getGrades() {
+        return mGrade;
+    }
+
+    public int getYear() {
+        return mYear;
+    }
+
+    // Constructor for ReportCard class
+    public ReportCard(String[] Subjects, char[] Grades, int Year) {
+        this.mSubject = Subjects;
+        this.mGrade = Grades;
+        this.mYear = Year;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public String toString() {
+        String result = "Year: " + mYear + "\n"
+                + "Subjects: " + Arrays.toString(mSubject) + "\n"
+                + "Grades: " + Arrays.toString(mGrade);
+        return result;
     }
 }
